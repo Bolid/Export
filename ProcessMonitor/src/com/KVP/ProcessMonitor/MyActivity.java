c31b963ab2526438d500fb400c915f63756093fa
bolid <dmitriy.grigorev@gmail.com>
2013-05-06 23:30:54 +0400

    Реализовано определение местоположения. Данные выводятся в тост и только один раз - при запуске

--- ProcessMonitor/src/com/KVP/ProcessMonitor/MyActivity.java
+++ ProcessMonitor/src/com/KVP/ProcessMonitor/MyActivity.java
@@ -3,10 +3,28 @@ package com.KVP.ProcessMonitor;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyActivity extends Activity {
     /**
@@ -20,6 +38,7 @@ public class MyActivity extends Activity {
         final Intent startServer = new Intent(getBaseContext(), Server.class);
         Button butPlay = (Button)findViewById(R.id.button);
         Button butStop = (Button)findViewById(R.id.button1);
+        Button butPost = (Button)findViewById(R.id.button2);
         butPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,5 +55,54 @@ public class MyActivity extends Activity {
                 server = null;
             }
         });
+        butPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Task task = new Task();
+                task.execute();
+            }
+        });
+    }
+    class Task extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... objects) {
+            try {
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost("http://wia-games.net/projects/appfield/api/api.php");
+                List<NameValuePair> parameters = new ArrayList<NameValuePair>(2);
+                JSONObject jsonObject = new JSONObject();
+                String json = "{\"dev_id\":\"1234\",\"model\":\"1234\",\"battery_level\":12,\"free_memory\":\"120\",\"os_locale\":\"ru-ru\",\"keyboard_locale\":\"ru-ru\",\"os_version\":17}";
+                //{"keyboard_locale":"ru-ru","battery_level":12,"model":"1234","os_locale":"ru-ru","free_memory":"120","dev_id":"1234","os_version":17}
+                //{"battery_level":12,"keyboard_locale":"ru-ru","model":"1234","free_memory":"120","os_locale":"ru-ru","dev_id":"1234","os_version":17}
+                jsonObject.put("keyboard_locale", "ru-ru");
+                jsonObject.put("battery_level", 12);
+                jsonObject.put("model", "1234");
+                jsonObject.put("os_locale", "en-us");
+                jsonObject.put("free_memory", 120);
+                jsonObject.put("dev_id", "1234767hjgjh");
+                jsonObject.put("os_version", 17);
+                parameters.add(new BasicNameValuePair("action", "update_user_info"));
+                parameters.add(new BasicNameValuePair("data", jsonObject.toString()));
+                httpPost.setEntity(new UrlEncodedFormEntity(parameters));
+
+                //httpPost.setHeader("Content-Type", "application/json");
+                httpPost.setHeader("Accept-Encoding", "application/json");
+                httpPost.setHeader("Accept-Language", "en-US");
+                HttpResponse httpResponse = httpClient.execute(httpPost);
+                Log.v("Server Application", EntityUtils.toString(httpResponse.getEntity())+" "+jsonObject.toString());
+
+            } catch (UnsupportedEncodingException e) {
+                Log.e("Server Application", "Error: " + e);
+            } catch (ClientProtocolException e) {
+                Log.e("Server Application", "Error: " + e);
+            } catch (IOException e) {
+                Log.e("Server Application", "Error: " + e);
+            } catch (JSONException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+
+            return null;
+        }
     }
 }
--- ProcessMonitor/src/com/KVP/ProcessMonitor/Server.java
+++ ProcessMonitor/src/com/KVP/ProcessMonitor/Server.java
@@ -6,10 +6,14 @@ import android.app.Notification;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.location.*;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Environment;
 import android.os.Process;
+import android.provider.Settings;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -27,6 +31,9 @@ public class Server extends IntentService {
     Calendar calendar = null;
     SimpleDateFormat pattern = null;
     String app = "com.KVP.ProcessMonitor";
+    LocationListener locationListener = null;
+    double lat;
+    double lot;
     public Server() {
         super("Server");
     }
@@ -37,6 +44,50 @@ public class Server extends IntentService {
         pattern = new SimpleDateFormat();
         pattern.applyPattern("dd-MM-yyyy HH-mm-ss");
         startForeground(1, new Notification());
+        final LocationManager locationManager =  (LocationManager)this.getSystemService(LOCATION_SERVICE);
+        final Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        locationListener = new LocationListener() {
+
+            @Override
+            public void onLocationChanged(Location location) {
+                lat = location.getLatitude();
+                lot = location.getLongitude();
+                Log.v(TAG, "Широта: " + location.getLatitude() + ". Долгота: " + location.getLongitude() + ".");try {
+                    List<Address> addresses = geocoder.getFromLocation(lat, lot, 1);
+                    Address address = addresses.get(0);
+                    Log.v(TAG, address.getAddressLine(0)+" "+address.getThoroughfare()+" "+address.getFeatureName() +" "+address.getSubAdminArea());
+                    String locationInfo = "Широта: "+lat+". Долгота: \b"+lot+".\nАдрес: " + address.getSubAdminArea() + ", " + address.getAddressLine(0) + ", " + address.getAdminArea() +", " + address.getCountryName() + ".\nКод страны: " + address.getCountryCode();
+                    Toast.makeText(getBaseContext(),locationInfo, Toast.LENGTH_LONG).show();
+                } catch (IOException ioe) {
+                    Log.e(TAG, "ERROR: " + ioe);
+                } catch (Exception e) {
+                    Log.e(TAG, "ERROR: " + e);
+                }
+                locationManager.removeUpdates(locationListener);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                try {
+                    Log.v(TAG, "Provider disabled. Started...");
+                    Intent startLocPropv = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startLocPropv.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    getBaseContext().startActivity(startLocPropv);
+                    Toast.makeText(getBaseContext(), "Please, enable provider network location.", Toast.LENGTH_LONG).show();
+                } catch (Exception e){
+                    Log.e(TAG, "ERROR: " + e);
+                }
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0,0, locationListener);
     }
 
 
@@ -50,6 +101,7 @@ public class Server extends IntentService {
         List <ActivityManager.RunningAppProcessInfo> listApplicationAll = null;
         List <ActivityManager.RunningAppProcessInfo> listApplicationNew = new ArrayList<ActivityManager.RunningAppProcessInfo>();
         List <Long> timeApplicationStart = new ArrayList <Long>();
+        List <String> timeApplicationStartStr = new ArrayList<String>();
         Long timeStop;
         systemDevice(packageManager);
         while (work){
@@ -58,22 +110,28 @@ public class Server extends IntentService {
             {
                 for (int i = 0; i < listApplicationNew.size(); i++){
                     String nameApplication = listApplicationNew.get(i).processName;
-                    Log.v(TAG, "Статус: " + listApplicationNew.get(i).importance);
                     for (int j = 0; j < listApplicationAll.size(); j++){
                         if (listApplicationAll.get(j).processName.equals(nameApplication) && listApplicationAll.get(j).importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_BACKGROUND){
                             calendar = Calendar.getInstance();
                             timeStop = Long.valueOf(calendar.get(Calendar.HOUR))*3600000 + Long.valueOf(calendar.get(Calendar.MINUTE))*60000 + Long.valueOf(calendar.get(Calendar.SECOND))*1000;
                             String timeWork = getTimeWork(timeApplicationStart.get(i), timeStop);
+                            nameApplication = getApplicationName(packageManager, listApplicationNew.get(i).processName);
+                            Log.v(TAG, "------------------------------------------------------------");
+                            Log.v(TAG, timeApplicationStartStr.get(i)+" Запустили процесс: " + listApplicationNew.get(i).processName + ". Название приложения: " + nameApplication);
                             Log.v(TAG, pattern.format(calendar.getTime())+" Закрыт процесс: " + listApplicationNew.get(i).processName);
                             Log.v(TAG, "\t\t\t\t\t\t\t\tПроцесс: " + listApplicationNew.get(i).processName+" работал: "+timeWork);
+                            Log.v(TAG, "============================================================");
                             //Log.v(TAG, "Статус приложения: " + listApplicationAll.get(j).importance);
-                            writeLog(null, pattern.format(calendar.getTime()).toString(), timeWork, listApplicationNew.get(i).processName, null);
+                            writeLog(timeApplicationStartStr.get(i), pattern.format(calendar.getTime()).toString(), timeWork, listApplicationNew.get(i).processName, nameApplication);
                             listApplicationNew.remove(i);
                             timeApplicationStart.remove(i);
+                            timeApplicationStartStr.remove(i);
+                            Log.v("label. ", "for-1 Label - 1");
                             if (listApplicationNew.isEmpty())
                                 break;
                         }else if (listApplicationAll.get(j).processName.equals(nameApplication) && listApplicationAll.get(j).importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
                             listApplicationAll.remove(j);
+                            Log.v("label. ", "for-1 Label - 2");
                         }
                     }
                 }
@@ -84,23 +142,18 @@ public class Server extends IntentService {
                             listApplicationAll.get(j).processName.equals("com.android.systemui") || listApplicationAll.get(j).processName.equals("com.android.launcher") ||
                             listApplicationAll.get(j).processName.equals("android.process.acore") || listApplicationAll.get(j).processName.equals("com.android.nfc") ||
                             listApplicationAll.get(j).processName.equals(getPackageName())){
+                        Log.v("label. ", "for-2 Label - 1");
                     }
                     else {
-                        String nameAppication = null;
-                        try {
-                            ai = (ApplicationInfo)getPackageManager().getApplicationInfo(listApplicationAll.get(j).processName, 0);
-                            nameAppication = packageManager.getApplicationLabel(ai).toString();
-                        }
-                        catch (PackageManager.NameNotFoundException pmnnfe) {
-                            Log.e(TAG, "Ошибка при получении информации о пакете: "+ pmnnfe);
-                        }
-                            Long timeStart = null;
-                            calendar = Calendar.getInstance();
-                            timeStart = Long.valueOf(calendar.get(Calendar.HOUR))*3600000 + Long.valueOf(calendar.get(Calendar.MINUTE))*60000 + Long.valueOf(calendar.get(Calendar.SECOND))*1000;
-                            timeApplicationStart.add(0, timeStart);
-                            listApplicationNew.add(0, listApplicationAll.get(j));
-                            Log.v(TAG, pattern.format(calendar.getTime()) + " Записали процесс: " + listApplicationAll.get(j).processName + " Название приложения: " + nameAppication);
-                            writeLog(pattern.format(calendar.getTime()).toString(), null, null, listApplicationAll.get(j).processName, nameAppication);
+                        Long timeStart = null;
+                        calendar = Calendar.getInstance();
+                        timeStart = Long.valueOf(calendar.get(Calendar.HOUR))*3600000 + Long.valueOf(calendar.get(Calendar.MINUTE))*60000 + Long.valueOf(calendar.get(Calendar.SECOND))*1000;
+                        timeApplicationStart.add(0, timeStart);
+                        timeApplicationStartStr.add(0, pattern.format(calendar.getTime()).toString());
+                        listApplicationNew.add(0, listApplicationAll.get(j));
+                        //Log.v(TAG, pattern.format(calendar.getTime()) + " Записали процесс: " + listApplicationAll.get(j).processName + " Название приложения: " + nameAppication);
+                        //writeLog(pattern.format(calendar.getTime()).toString(), null, null, listApplicationAll.get(j).processName, nameAppication);
+                        Log.v("label. ", "for-2 Label - 2");
 
                     }
                 }
@@ -175,20 +228,30 @@ public class Server extends IntentService {
     public void writeLog(String timeStart, String timeStop, String timeWork, String nameProcess, String nameApplication){
         try {
             FileWriter fileWriter = new FileWriter(Environment.getExternalStorageDirectory() + "/NewApplicationStart.log", true);
-            if (timeStart != null){
-                fileWriter.append("------------------------------\n");
-                fileWriter.append(timeStart + " Запустили процесс: " + nameProcess + ". Название приложения: " + nameApplication + ".\n");
-            }
-            else if (timeStop != null){
-                fileWriter.append(timeStop + " Закрыли процесс: " + nameProcess + ".\n");
-                fileWriter.append("\t\t\t\t\t\t\t\tПриложение работало: " + timeWork + ".\n");
-            }
+            fileWriter.append("------------------------------------------------------------\n");
+            fileWriter.append(timeStart + " Запустили процесс: " + nameProcess + ". Название приложения: " + nameApplication + ".\n");
+            fileWriter.append(timeStop + " Закрыли процесс: " + nameProcess + ".\n");
+            fileWriter.append("\t\t\t\t\t\t\t\tПриложение работало: " + timeWork + ".\n");
+            fileWriter.append("============================================================\n");
             fileWriter.close();
             fileWriter = null;
         } catch (IOException ioe) {
             Log.e(TAG, "Ошибка при открытии файла лога: ", ioe);
         }
-
-
     }
+
+    public String getApplicationName(PackageManager packageManager, String processName){
+        ApplicationInfo ai = null;
+        try {
+            ai = (ApplicationInfo)getPackageManager().getApplicationInfo(processName, 0);
+            processName = packageManager.getApplicationLabel(ai).toString();
+        }
+        catch (PackageManager.NameNotFoundException pmnnfe) {
+            Log.e(TAG, "Ошибка при получении информации о пакете: "+ pmnnfe);
+            processName = null;
+        }
+        return processName;
+    }
+
+
 }
